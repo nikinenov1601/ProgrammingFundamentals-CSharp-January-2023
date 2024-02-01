c0754fe03ef8342042588044b7b52ed737ee7938
nikinenov1601 <nikinenov1@abv.bg>
2024-02-02 00:31:15 +0200

    Adding eleventh problem solution "Orders"

---
+++ 2.Basic-Syntax-Conditional-Statements-And-Loops-Exercise/11.Orders/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+int orders = int.Parse(Console.ReadLine());
+double totalPrice = 0;
+for (int i = 0; i < orders; i++)
+{
+    double pricePerCapsule = double.Parse(Console.ReadLine());
+    int daysInMonth = int.Parse(Console.ReadLine());
+    int capsuleCount = int.Parse(Console.ReadLine());
+
+    double price = ((daysInMonth * capsuleCount) * pricePerCapsule);
+    Console.WriteLine($"The price for the coffee is: ${price:f2}");
+
+    totalPrice += price;
+}
+Console.WriteLine($"Total: ${totalPrice:f2}");
