cd7e6018099fda45cfec9c55b813c4f90961d1cc
nikinenov1601 <nikinenov1@abv.bg>
2024-02-02 00:28:56 +0200

    Adding ninth problem solution "PadawanEquipment"

---
+++ 2.Basic-Syntax-Conditional-Statements-And-Loops-Exercise/09.PadawanEquipment/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+float moneyAvailable = float.Parse(Console.ReadLine());
+
+int studetsCount = int.Parse(Console.ReadLine());
+float lightsaberPrice = float.Parse(Console.ReadLine());
+float robesPrice = float.Parse(Console.ReadLine());
+float beltsPrice = float.Parse(Console.ReadLine());
+
+lightsaberPrice *=(float)Math.Ceiling(studetsCount * 1.1f);
+robesPrice *= studetsCount;
+beltsPrice *= (studetsCount - studetsCount / 6);
+
+double cost = lightsaberPrice + robesPrice + beltsPrice;
+
+if (moneyAvailable >= cost)
+{
+    Console.WriteLine($"The money is enough - it would cost {cost:f2}lv.");
+}
+else
+{
+    Console.WriteLine($"John will need {cost - moneyAvailable:f2}lv more.");
+}
