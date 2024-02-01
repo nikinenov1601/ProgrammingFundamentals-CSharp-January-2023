02550979d52916838c0f1f33b86606e75d87bf91
nikinenov1601 <nikinenov1@abv.bg>
2024-02-02 00:24:09 +0200

    Adding sixth problem solution StrongNumber

---
+++ 2.Basic-Syntax-Conditional-Statements-And-Loops-Exercise/06.StrongNumber/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+int number = int.Parse(Console.ReadLine());
+
+int numberCopy = number;
+
+int sum = 0;
+
+while (numberCopy > 0)
+{
+    int digit = numberCopy % 10;
+    numberCopy /= 10;
+    int factoriel = 1;
+
+    for (int i = 1; i <= digit; i++)
+    {
+        factoriel *= i;
+    }
+
+    sum += factoriel;
+}
+
+if (number == sum)
+{
+    Console.WriteLine("yes");
+}
+else
+{
+    Console.WriteLine("no");
+}
