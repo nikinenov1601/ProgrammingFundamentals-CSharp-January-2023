bb5a2971df0a588088ca6be06a797edfdf7da363
nikinenov1601 <nikinenov1@abv.bg>
2024-02-02 00:22:23 +0200

    Adding fifth problem solution "Login"

---
+++ 2.Basic-Syntax-Conditional-Statements-And-Loops-Exercise/05.Login/Program.cs
@@ -0,0 +1,28 @@
+﻿
+string username = Console.ReadLine();
+string correctPasssword = string.Empty;
+string passwordAttempt = Console.ReadLine();
+int incorrectPasswordAttempts = 0;
+
+for (int i = username.Length-1; i >= 0; i--)
+{
+    correctPasssword += username[i];
+}
+
+while (passwordAttempt != correctPasssword)
+{
+    Console.WriteLine("Incorrect password. Try again.");
+    passwordAttempt = Console.ReadLine();
+    incorrectPasswordAttempts++;
+
+    if (incorrectPasswordAttempts == 3)
+    {
+        Console.WriteLine($"User {username} blocked!");
+        break;
+    }
+    if (passwordAttempt == correctPasssword)
+    {
+        Console.WriteLine($"User {username} logged in.");
+        break;
+    }
+}
