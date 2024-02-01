e7adb97c3a54d729e900f8c8ba6b3a6962dd146a
nikinenov1601 <nikinenov1@abv.bg>
2024-02-02 00:30:20 +0200

    Adding tenth problem solution "RageExpenses"

---
+++ 2.Basic-Syntax-Conditional-Statements-And-Loops-Exercise/10.RageExpenses/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+int number = int.Parse(Console.ReadLine());
+
+float headsetPrice = float.Parse(Console.ReadLine());
+float mousetPrice = float.Parse(Console.ReadLine());
+float keyboardPrice = float.Parse(Console.ReadLine());
+float displayPrice = float.Parse(Console.ReadLine());
+
+int headset = number / 2;
+int mouse = number / 3;
+int keyboard = number / 6;
+int display = number / 12;
+
+float totalRageExpenses = headset * headsetPrice + mouse * mousetPrice + keyboard * keyboardPrice + display * displayPrice;
+
+Console.WriteLine($"Rage expenses: {totalRageExpenses:f2} lv.");
