1c4a693e8ab7c8393541502ea7e0c5062d951c73
nikinenov1601 <nikinenov1@abv.bg>
2024-02-03 00:38:43 +0200

    Adding solution to seventh problem 'EqualArrays'

---
+++ 03.Arrays - Lab/07.EqualArrays/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+int[] firstArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int[] secondArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+bool areEqual = true;
+
+for (int i = 0; i < firstArray.Length; i++)
+{
+
+    if (firstArray[i] != secondArray[i])
+    {
+        Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+        areEqual = false;
+        break;
+    }
+}
+if (areEqual)
+{
+    int sum = firstArray.Sum();
+    Console.WriteLine($"Arrays are identical. Sum: {sum}");
+}
