3bbcd3ed86a9213243d0ecca5a66e0fd1d7012a0
nikinenov1601 <nikinenov1@abv.bg>
2024-02-03 00:39:45 +0200

    Adding solution to eight problem 'CondenseArrayToNumber'

---
+++ 03.Arrays - Lab/08.CondenseArrayToNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿
+int[] arrayOfNumbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+for (int i = 0; i < arrayOfNumbers.Length - 1; i++)
+{
+    for (int j = 0; j < arrayOfNumbers.Length - 1 - i; j++)
+    {
+        arrayOfNumbers[j] = arrayOfNumbers[j] + arrayOfNumbers[j + 1];
+
+    }
+}
+Console.WriteLine(arrayOfNumbers[0]);
