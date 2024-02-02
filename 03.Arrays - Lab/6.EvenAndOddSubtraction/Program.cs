e6b0d297dbe2e42f3f8f7ee31a091fe922f34c8e
nikinenov1601 <nikinenov1@abv.bg>
2024-02-03 00:37:07 +0200

    Adding solution to sixth problem 'EvenAndOddSubtraction'

---
+++ 03.Arrays - Lab/6.EvenAndOddSubtraction/Program.cs
@@ -0,0 +1,9 @@
+﻿
+
+int[] arrayOfNumbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+int oddNumberSum = arrayOfNumbers.Where(x => x % 2 != 0).Sum();
+
+int evenNumberSum = arrayOfNumbers.Where(x => x % 2 == 0).Sum();
+
+Console.WriteLine(evenNumberSum - oddNumberSum);
