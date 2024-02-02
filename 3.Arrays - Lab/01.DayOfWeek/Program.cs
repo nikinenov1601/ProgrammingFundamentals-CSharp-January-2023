501891ef9ea6294196f185c4c583ad1581b385b3
nikinenov1601 <nikinenov1@abv.bg>
2024-02-03 00:30:07 +0200

    Adding solution to first and second problem "Day of Week' & 'Print Numbers in Reverse Order'

---
+++ 3.Arrays - Lab/01.DayOfWeek/Program.cs
@@ -0,0 +1,12 @@
+﻿
+string[] dayOfTheWeek = { "Invalid day!", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+int number = int.Parse(Console.ReadLine());
+
+if (number >= 1 && number <= 7)
+{
+    Console.WriteLine(dayOfTheWeek[number]);
+}
+else
+{
+    Console.WriteLine(dayOfTheWeek[0]);
+}
---
+++ 3.Arrays - Lab/02.PrintNumbersInReverseOrder/Program.cs
@@ -0,0 +1,11 @@
+﻿
+int count = int.Parse(Console.ReadLine());
+int[] arrayOfNumbers = new int[count];
+
+for (int i = 0; i < count; i++)
+{
+    int number = int.Parse(Console.ReadLine());
+    arrayOfNumbers[i] = number;
+}
+Array.Reverse(arrayOfNumbers);
+Console.WriteLine(String.Join(" ", arrayOfNumbers));
