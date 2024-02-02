513248e97097bf0460672aff6adce1c09c6c8e69
nikinenov1601 <nikinenov1@abv.bg>
2024-02-03 00:35:32 +0200

    Adding solution to fifth problem 'SumEvenNumbers'

---
+++ 03.Arrays - Lab/05.SumEvenNumbers/Program.cs
@@ -0,0 +1,10 @@
+﻿
+int[] arrayOfNumbers = Console.ReadLine().Split().Select(int.Parse).Where(x=> x % 2 == 0).ToArray();
+
+int sum = 0;
+
+for (int i = 0; i < arrayOfNumbers.Length; i++)
+{
+    sum += arrayOfNumbers[i];
+}
+Console.WriteLine(sum);
