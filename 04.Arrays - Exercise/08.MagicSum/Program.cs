d8e37882402a65bdd6f52a2f8b1869b938ac5cda
nikinenov1601 <nikinenov1@abv.bg>
2024-02-04 18:59:04 +0200

    Adding solution to eight problem

---
+++ 04.Arrays - Exercise/08.MagicSum/Program.cs
@@ -0,0 +1,20 @@
+﻿string[] input = Console.ReadLine().Split(' ');
+int[] numbers = new int[input.Length];
+
+for (int i = 0; i < input.Length; i++)
+{
+    numbers[i] = int.Parse(input[i]);
+}
+
+int targetSum = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    for (int j = i + 1; j < numbers.Length; j++)
+    {
+        if (numbers[i] + numbers[j] == targetSum)
+        {
+            Console.WriteLine($"{numbers[i]} {numbers[j]}");
+        }
+    }
+}
