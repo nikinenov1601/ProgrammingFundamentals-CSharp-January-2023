f4633849071241fc520d18ad1e5806495c5a8f54
nikinenov1601 <nikinenov1@abv.bg>
2024-02-04 18:49:27 +0200

    Adding the first two problem solutions

---
+++ 04.Arrays - Exercise/01.Train/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+int n = int.Parse(Console.ReadLine());
+int[] arrayOfNumbers = new int[n];
+
+for (int i = 0; i < n; i++)
+{
+    int addedNumber = int.Parse(Console.ReadLine());
+    arrayOfNumbers[i] = addedNumber;
+}
+Console.WriteLine(String.Join(" ", arrayOfNumbers));
+Console.WriteLine(arrayOfNumbers.Sum());
