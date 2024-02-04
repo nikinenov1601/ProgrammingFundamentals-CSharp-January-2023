3b3561c8e51ae6e84bd34d62cee458d681211675
nikinenov1601 <nikinenov1@abv.bg>
2024-02-04 18:52:54 +0200

    Adding third problem solution

---
+++ 04.Arrays - Exercise/03.Zig-ZagArrays/Program.cs
@@ -0,0 +1,22 @@
+﻿
+int number = int.Parse(Console.ReadLine());
+int[] firstArray = new int[number];
+int[] secondArray = new int[number];
+
+for (int i = 0; i < number; i++)
+{
+    int[] arrayOfNumbersToAdd = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+    if (i % 2 == 0)
+    {
+        firstArray[i] = arrayOfNumbersToAdd[0];
+        secondArray[i] = arrayOfNumbersToAdd[1];
+    }
+    else
+    {
+        firstArray[i] = arrayOfNumbersToAdd[1];
+        secondArray[i] = arrayOfNumbersToAdd[0];
+    }
+}
+    Console.WriteLine(String.Join(" ", firstArray));
+    Console.WriteLine(String.Join(" ", secondArray));
