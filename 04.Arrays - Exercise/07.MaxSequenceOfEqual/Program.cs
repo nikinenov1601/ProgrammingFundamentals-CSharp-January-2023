65c57ab21d9f80da12035581a61df6c105d9dcea
nikinenov1601 <nikinenov1@abv.bg>
2024-02-04 18:58:17 +0200

    Adding solution to seventh problem

---
+++ 04.Arrays - Exercise/07.MaxSequenceOfEqual/Program.cs
@@ -0,0 +1,26 @@
+﻿int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+int longestSequenceStart = 0;
+int longestSequenceLenght = 0;
+
+
+for (int i = 1; i < numbers.Length; i++)
+{
+    int currentSequence = i - 1;
+    int currentSequenceLenght = 1;
+
+    while (i < numbers.Length && numbers[i] == numbers[i - 1])
+    {
+        currentSequenceLenght++;
+        i++;
+    }
+    if (currentSequenceLenght > longestSequenceLenght)
+    {
+        longestSequenceLenght = currentSequenceLenght;
+        longestSequenceStart = currentSequence;
+    }
+}
+for (int i = longestSequenceStart; i < longestSequenceStart + longestSequenceLenght; i++)
+{
+    Console.Write($"{numbers[i]} ");
+}
