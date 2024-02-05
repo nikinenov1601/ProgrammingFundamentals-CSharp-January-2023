8917b499eb99b557f13ae00618fad09b848395ac
nikinenov1601 <nikinenov1@abv.bg>
2024-02-05 20:39:05 +0200

    Adding fifth problem solution to Methods - Lab

---
+++ 05.Methods - Lab/05.Orders/Program.cs
@@ -0,0 +1,41 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        string product = Console.ReadLine();
+        int quantity = int.Parse(Console.ReadLine());
+
+        if (product == "coffee")
+        {
+            Coffee(quantity);
+        }
+        else if (product == "water")
+        {
+            Water(quantity);
+        }
+        else if (product == "coke")
+        {
+            Coke(quantity);
+        }
+        else if (product == "snacks")
+        {
+            Snacks(quantity);
+        }
+    }
+    static void Coffee(double n)
+    {
+        Console.WriteLine($"{n * 1.50:f2}");
+    }
+    static void Water(double n)
+    {
+        Console.WriteLine($"{n * 1.00:f2}");
+    }
+    static void Coke(double n)
+    {
+        Console.WriteLine($"{n * 1.40:f2}");
+    }
+    static void Snacks(double n)
+    {
+        Console.WriteLine($"{n * 2.00:f2}");
+    }
+}
