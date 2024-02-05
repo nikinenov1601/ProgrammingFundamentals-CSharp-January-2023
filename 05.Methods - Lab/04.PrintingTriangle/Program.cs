6df57a68594bf0c1db1fb7108d7c3a17c404fbe7
nikinenov1601 <nikinenov1@abv.bg>
2024-02-05 20:38:35 +0200

    Adding fourth problem solution to Methods - Lab

---
+++ 05.Methods - Lab/04.PrintingTriangle/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= n; i++)
+        {
+            Method(1, i);
+        }
+        for (int i = n - 1; i >= 1; i--)
+        {
+            Method(1, i);
+        }
+    }
+    static void Method(int start, int end)
+    {
+        for (int i = start; i <= end; i++)
+        {
+            Console.Write(i + " ");
+        }
+        Console.WriteLine();
+    }
+}
