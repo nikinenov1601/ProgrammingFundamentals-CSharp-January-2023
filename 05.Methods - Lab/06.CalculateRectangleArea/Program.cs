9ebb323267fe67b11be5a3d25a0ece09bea1b75b
nikinenov1601 <nikinenov1@abv.bg>
2024-02-05 20:39:36 +0200

    Adding sixth problem solution to Methods - Lab

---
+++ 05.Methods - Lab/06.CalculateRectangleArea/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        double width = double.Parse(Console.ReadLine());
+        double height = double.Parse(Console.ReadLine());
+        double area = GetRectangleArea(width, height);
+        Console.WriteLine(area);
+
+    }
+    static double GetRectangleArea(double width, double height)
+    {
+        return width * height;
+    }
+}
