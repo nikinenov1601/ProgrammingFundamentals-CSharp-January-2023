95c6f298feb0d0d0f00a62287c2b49baf9882680
nikinenov1601 <nikinenov1@abv.bg>
2024-02-05 20:41:55 +0200

    Adding tenth problem solution to Methods - Lab

---
+++ 05.Methods - Lab/10.MultiplyEvensByOdds/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace EvenOddMultiplier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(GetMultipleOfEvenAndOdds(number));
+            Console.ReadLine();
+        }
+
+        static int GetMultipleOfEvenAndOdds(int number)
+        {
+            int sumOfEven = GetSumOfEvenDigits(number);
+            int sumOfOdd = GetSumOfOddDigits(number);
+
+            return sumOfEven * sumOfOdd;
+        }
+
+        static int GetSumOfEvenDigits(int number)
+        {
+            int sum = 0;
+            number = Math.Abs(number);
+
+            while (number > 0)
+            {
+                int digit = number % 10;
+                if (digit % 2 == 0)
+                {
+                    sum += digit;
+                }
+
+                number /= 10;
+            }
+
+            return sum;
+        }
+
+        static int GetSumOfOddDigits(int number)
+        {
+            int sum = 0;
+            number = Math.Abs(number);
+
+            while (number > 0)
+            {
+                int digit = number % 10;
+                if (digit % 2 != 0)
+                {
+                    sum += digit;
+                }
+
+                number /= 10;
+            }
+
+            return sum;
+        }
+    }
+}
