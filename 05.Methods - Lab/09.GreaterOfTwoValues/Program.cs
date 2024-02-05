00ae0df6251f6eaef6d5801d78808996276cf630
nikinenov1601 <nikinenov1@abv.bg>
2024-02-05 20:41:20 +0200

    Adding ninth problem solution to Methods - Lab

---
+++ 05.Methods - Lab/09.GreaterOfTwoValues/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.Runtime.ExceptionServices;
+
+internal class Program
+{
+    static void Main()
+    {
+        string command = Console.ReadLine();
+
+        if (command == "int")
+        {
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+            GetMax(first, second);
+        }
+        else if (command == "char")
+        {
+            char first = char.Parse(Console.ReadLine());
+            char second = char.Parse(Console.ReadLine());
+            GetMax(first, second);
+        }
+        else if (command == "string")
+        {
+            string first = Console.ReadLine();
+            string second = Console.ReadLine();
+            GetMax(first, second);
+        }
+    }
+    static int GetMax(int first, int second)
+    {
+        int result = 0;
+
+        if (first > second)
+        {
+            result = first;
+        }
+        else
+        {
+            result = second;
+        }
+        Console.WriteLine(result);
+        return result;
+    }
+    static char GetMax(char first, char second)
+    {
+        int result;
+
+        if (first > second)
+        {
+            result = first;
+        }
+        else
+        {
+            result = second;
+        }
+        Console.WriteLine((char)result);
+        return (char)result;
+    }
+    static string GetMax(string first, string second)
+    {
+        string result = string.Empty;
+        int comparison = first.CompareTo(second);
+        if (comparison > 0)
+        {
+            result = first;
+        }
+        else
+        {
+            result = second;
+        }
+        Console.WriteLine(result);
+        return (result);
+    }
+}
