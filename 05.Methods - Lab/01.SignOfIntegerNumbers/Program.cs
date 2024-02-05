8366f240963f6bbdb031a70c8ee9c40746b06442
nikinenov1601 <nikinenov1@abv.bg>
2024-02-05 20:36:26 +0200

    Adding first problem solution to Methods - Lab

---
+++ 05.Methods - Lab/01.SignOfIntegerNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+internal class Program
+{
+    static void Main()
+    {
+        int number = int.Parse(Console.ReadLine());
+        PrintNumber(number);
+    }
+    static void PrintNumber(int number)
+    {
+        if (number > 0)
+        {
+            Console.WriteLine($"The number {number} is positive. ");
+        }
+        else if (number < 0)
+        {
+            Console.WriteLine($"The number {number} is negative. ");
+        }
+        else if (number == 0)
+        {
+            Console.WriteLine($"The number {number} is zero. ");
+        }
+    }
+}
