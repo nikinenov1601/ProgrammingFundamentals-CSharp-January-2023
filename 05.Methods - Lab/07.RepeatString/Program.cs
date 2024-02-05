ee79f6c6740b4a5aacf82d25f286ffe54c9d3ccf
nikinenov1601 <nikinenov1@abv.bg>
2024-02-05 20:40:02 +0200

    Adding seventh problem solution to Methods - Lab

---
+++ 05.Methods - Lab/07.RepeatString/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        int n = int.Parse(Console.ReadLine());
+
+        RepeatString(input, n);
+    }
+    private static string RepeatString(string str, int count)
+    {
+        string result = "";
+        for (int i = 0; i < count; i++)
+        {
+            Console.Write(str + "");
+        }
+        return result;
+    }
+}
