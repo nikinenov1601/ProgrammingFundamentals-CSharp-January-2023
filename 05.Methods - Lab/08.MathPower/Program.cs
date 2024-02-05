aaecc817203734d0ecffae9924d2f0570f15824a
nikinenov1601 <nikinenov1@abv.bg>
2024-02-05 20:40:41 +0200

    Adding seventh problem solution to Methods - Lab

---
+++ 05.Methods - Lab/08.MathPower/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        double number = double.Parse(Console.ReadLine());
+        int power = int.Parse(Console.ReadLine());
+        double finalResult = RaiseToPower(number, power);
+        Console.WriteLine(finalResult);
+    }
+    static double RaiseToPower(double number, int power)
+    {
+        double result = 0d;
+        result = Math.Pow(number, power);
+        return result;
+    }
+}
