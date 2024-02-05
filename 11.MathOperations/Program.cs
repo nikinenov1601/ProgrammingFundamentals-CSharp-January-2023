e285d0ef7f13152a6f43afdf922bc0179cebeac6
nikinenov1601 <nikinenov1@abv.bg>
2024-02-05 20:42:33 +0200

    Adding eleventh problem solution to Methods - Lab

---
+++ 11.MathOperations/Program.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        int firstNumber = int.Parse(Console.ReadLine());
+        string stringOperator = Console.ReadLine();
+        int secondNumber = int.Parse(Console.ReadLine());
+        Calculate(firstNumber, stringOperator, secondNumber);
+
+    }
+    private static double Calculate(int firstNumber, string Operator, int secondNumber)
+    {
+        double result = 0;
+
+        switch (Operator)
+        {
+            case "/":
+                result = firstNumber / secondNumber;
+                break;
+            case "*":
+                result = firstNumber * secondNumber;
+                break;
+            case "+":
+                result = firstNumber + secondNumber;
+                break;
+            case "-":
+                result = firstNumber - secondNumber;
+                break;
+        }
+        Console.WriteLine(result);
+        return result;
+    }
+}
