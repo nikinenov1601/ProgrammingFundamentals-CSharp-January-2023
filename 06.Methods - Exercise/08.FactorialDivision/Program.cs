c561205eee6835f60aa71e17dc34dafc1a743625
nikinenov1601 <nikinenov1@abv.bg>
2024-02-06 17:35:31 +0200

    Adding eight problem solution to Methods exercise

---
+++ 06.Methods - Exercise/08.FactorialDivision/Program.cs
@@ -0,0 +1,30 @@
+﻿double firstNumber = double.Parse(Console.ReadLine());
+double secondNumber = double.Parse(Console.ReadLine());
+
+double factorialFirstNumber = FactorialFirstMethod(firstNumber);
+double factorialSecondNumber = FactorialSecondMethord(secondNumber);
+
+double factorialSubstract = factorialFirstNumber / factorialSecondNumber;
+Console.WriteLine($"{factorialSubstract:f2}");
+
+double FactorialFirstMethod(double firstNumber)
+{
+    double factorialFirst = 1;
+
+    for (int i = 1; i <= firstNumber; i++)
+    {
+        factorialFirst *= i;
+    }
+    return factorialFirst;
+}
+
+double FactorialSecondMethord(double secondNumber)
+{
+    double factorialSecond = 1;
+
+    for (int i = 1; i <= secondNumber; i++)
+    {
+        factorialSecond *= i;
+    }
+    return factorialSecond;
+}
