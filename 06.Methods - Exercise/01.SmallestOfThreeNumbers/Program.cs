d1a517196ff52fb7cf672534f8d7d8f4d0130481
nikinenov1601 <nikinenov1@abv.bg>
2024-02-06 17:30:18 +0200

    Adding first problem solution to Methods exercise

---
+++ 06.Methods - Exercise/01.SmallestOfThreeNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+int firstNumber = int.Parse(Console.ReadLine());
+int secondNumber = int.Parse(Console.ReadLine());
+int thirdNumber = int.Parse(Console.ReadLine());
+
+int smallestNumber = PrintingOutSmallestNumber(firstNumber, secondNumber, thirdNumber);
+
+int PrintingOutSmallestNumber(int firstNumber, int secondNumber, int thirdNumber)
+{
+    int smallestNumber = 0;
+    if (firstNumber <= secondNumber && firstNumber <= thirdNumber)
+    {
+        smallestNumber = firstNumber;
+    }
+    else if (secondNumber <= firstNumber && secondNumber <= thirdNumber)
+    {
+        smallestNumber = secondNumber;
+    }
+    else if (thirdNumber <= firstNumber && thirdNumber <= secondNumber)
+    {
+        smallestNumber = thirdNumber;
+    }
+    Console.WriteLine(smallestNumber);
+    return smallestNumber;
+}
