7f8140e283750e8d6bce450a6527b9909e69f0b4
nikinenov1601 <nikinenov1@abv.bg>
2024-02-06 17:32:03 +0200

    Adding third problem solution to Methods exercise

---
+++ 06.Methods - Exercise/03.CharactersInRange/Program.cs
@@ -0,0 +1,13 @@
+﻿char first = char.Parse(Console.ReadLine());
+char second = char.Parse(Console.ReadLine());
+
+if (first > second)
+{
+    char temp = first;
+    first = second;
+    second = temp;
+}
+for (int i = first + 1; i < second; i++)
+{
+    Console.Write((char)i + " ");
+}
