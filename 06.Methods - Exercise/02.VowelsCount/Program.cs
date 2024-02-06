87436db0b35662f16bafffcf50532cba0b57a7c4
nikinenov1601 <nikinenov1@abv.bg>
2024-02-06 17:31:23 +0200

    Adding second problem solution to Methods exercise

---
+++ 06.Methods - Exercise/02.VowelsCount/Program.cs
@@ -0,0 +1,20 @@
+﻿string input = Console.ReadLine();
+
+int vowelsCount = PrintingOutNumberOfVowels(input);
+
+int PrintingOutNumberOfVowels(string input)
+{
+    int vowelsCount = 0;
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        char letter = input[i];
+
+        if (letter == 'a' || letter == 'y' || letter == 'o' || letter == 'u' || letter == 'e' || letter == 'i' || letter == 'A' || letter == 'Y' || letter == 'O' || letter == 'U' || letter == 'E' || letter == 'I')
+        {
+            vowelsCount++;
+        }
+    }
+    Console.WriteLine(vowelsCount);
+    return vowelsCount;
+}
