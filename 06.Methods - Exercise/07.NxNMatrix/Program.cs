87b61439e282dcda5b15dd12a30659be2263d814
nikinenov1601 <nikinenov1@abv.bg>
2024-02-06 17:34:55 +0200

    Adding seventh problem solution to Methods exercise

---
+++ 06.Methods - Exercise/07.NxNMatrix/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+int input = int.Parse(Console.ReadLine());
+
+PrintRow(input);
+
+static void PrintRow(int input)
+{
+    for (int i = 0; i < input; i++)
+    {
+        PrintColumns(input);
+    }
+}
+
+static void PrintColumns(int input)
+{
+    for (int j = 0; j < input; j++)
+    {
+        Console.Write(input + " ");
+    }
+    Console.WriteLine();
+}
