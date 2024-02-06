954c3ddb1f1224f24de81a62105b0d5fdec415d7
nikinenov1601 <nikinenov1@abv.bg>
2024-02-06 17:33:40 +0200

    Adding fifth problem solution to Methods exercise

---
+++ 06.Methods - Exercise/05.AddAndSubtract/Program.cs
@@ -0,0 +1,19 @@
+﻿
+int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+int num3 = int.Parse(Console.ReadLine());
+
+int resultSum = Sum(num1, num2);
+int finalResult = SubtractFromSum(resultSum, num3);
+
+Console.WriteLine(finalResult);
+
+int Sum(int num1, int num2)
+{
+    return num1 + num2;
+}
+
+int SubtractFromSum(int resultSum, int num3)
+{
+    return resultSum - num3;
+}
