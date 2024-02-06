fcade25cbb9d2a1314a7e1e0087f57b436eff7e9
nikinenov1601 <nikinenov1@abv.bg>
2024-02-06 17:34:20 +0200

    Adding sixth problem solution to Methods exercise

---
+++ 06.Methods - Exercise/06.MiddleCharacters/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+string input = Console.ReadLine();
+Console.WriteLine(GetMidCharacter(input));
+
+static string GetMidCharacter(string input)
+{
+    string result = string.Empty;
+
+    if (input.Length % 2 == 1)
+    {
+        result = input[input.Length / 2].ToString();
+    }
+    else
+    {
+        result = input[input.Length / 2-1] + input[input.Length / 2].ToString();
+    }
+
+    return result;
+}
