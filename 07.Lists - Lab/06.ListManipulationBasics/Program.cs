a98b4bd36132f636792ea6f7b797369886875833
nikinenov1601 <nikinenov1@abv.bg>
2024-02-07 21:31:16 +0200

    Adding third problem solution to Lists Lab

---
+++ 07.Lists - Lab/06.ListManipulationBasics/Program.cs
@@ -0,0 +1,35 @@
+﻿List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+while (true)
+{
+    string line = Console.ReadLine();
+
+    if (line == "end")
+    {
+        break;
+    }
+
+    string[] token = line.Split();
+
+    if (token[0] == "Add")
+    {
+        numbers.Add(int.Parse(token[1]));
+    }
+    else if (token[0] == "Remove")
+    {
+        numbers.Remove(int.Parse(token[1]));
+    }
+    else if (token[0] == "RemoveAt")
+    {
+        numbers.RemoveAt(int.Parse(token[1]));
+    }
+    else if (token[0] == "Insert")
+    {
+        int NumberToInsert = int.Parse(token[1]);
+        int IndexToInsert = int.Parse(token[2]);
+
+        numbers.Insert(IndexToInsert, NumberToInsert);
+    }
+}
+
+Console.WriteLine(string.Join(" ", numbers));
