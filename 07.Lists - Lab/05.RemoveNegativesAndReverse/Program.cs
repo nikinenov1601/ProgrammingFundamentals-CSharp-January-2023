3519fe79309ba0c61fa41ce6769bf8e70b1ab55a
nikinenov1601 <nikinenov1@abv.bg>
2024-02-07 21:30:43 +0200

    Adding third problem solution to Lists Lab

---
+++ 07.Lists - Lab/05.RemoveNegativesAndReverse/Program.cs
@@ -0,0 +1,19 @@
+﻿List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+for (int i = 0; i < numbers.Count; i++)
+{
+    if (numbers[i] < 0)
+    {
+        numbers.RemoveAt(i);
+        i = -1;
+    }
+}
+if (numbers.Count == 0)
+{
+    Console.WriteLine("empty");
+}
+else
+{
+    numbers.Reverse();
+    Console.WriteLine(string.Join(" ", numbers));
+}
