c0e9425cd802dc21b8f9a8731b96e569ff706d89
nikinenov1601 <nikinenov1@abv.bg>
2024-02-08 22:45:12 +0200

    Adding seventh problem solution to Lists Exercise

---
+++ 08.Lists - Exercise/07.AppendArrays/Program.cs
@@ -0,0 +1,4 @@
+﻿string[] input = Console.ReadLine().Split('|').Reverse().ToArray();
+string temporal = string.Join(" ", input);
+string[] result = temporal.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+Console.WriteLine(string.Join(" ", result));
