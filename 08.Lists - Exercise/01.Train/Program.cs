b1a3a3d8c2c9e3e44fc9688cba8afae8d4668481
nikinenov1601 <nikinenov1@abv.bg>
2024-02-08 22:40:43 +0200

    Adding first problem solution

---
+++ 08.Lists - Exercise/01.Train/Program.cs
@@ -0,0 +1,31 @@
+﻿List<int> wagons = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+int maxCapacityofWagon = int.Parse(Console.ReadLine());
+
+string input = Console.ReadLine();
+
+while (input != "end")
+{
+    string[] commad = input.Split(" ");
+
+    if (commad.Length == 2)
+    {
+        int passenger = int.Parse(commad[1]);
+        wagons.Add(passenger);
+    }
+    else
+    {
+        int passenger = int.Parse(commad[0]);
+
+        for (int i = 0; i < wagons.Count; i++)
+        {
+            if (wagons[i] + passenger <= maxCapacityofWagon)
+            {
+                wagons[i] += passenger;
+                break;
+            }
+        }
+    }
+    input = Console.ReadLine();
+}
+Console.WriteLine(string.Join(" ", wagons));
