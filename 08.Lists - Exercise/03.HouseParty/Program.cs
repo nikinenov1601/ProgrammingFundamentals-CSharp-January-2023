ec6c5d4bf71cab6d278065ea2702c56d04ec89f5
nikinenov1601 <nikinenov1@abv.bg>
2024-02-08 22:42:14 +0200

    Adding third problem solution to Lists Exercise

---
+++ 08.Lists - Exercise/03.HouseParty/Program.cs
@@ -0,0 +1,33 @@
+﻿int count = int.Parse(Console.ReadLine());
+
+List<string> guestList = new List<string>();
+
+for (int i = 0; i < count; i++)
+{
+    string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    string name = input[0];
+
+    if (input.Length == 3)
+    {
+        if (guestList.Contains(name))
+        {
+            Console.WriteLine($"{name} is already in the list!");
+        }
+        else
+        {
+            guestList.Add(name);
+        }
+    }
+    else
+    {
+        if (!guestList.Contains(name))
+        {
+            Console.WriteLine($"{name} is not in the list!");
+        }
+        else
+        {
+            guestList.Remove(name);
+        }
+    }
+}
+Console.WriteLine(string.Join(Environment.NewLine, guestList));
