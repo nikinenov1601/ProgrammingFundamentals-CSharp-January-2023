cca77c49862e28a8e902049a7f3cbda3267ce2e9
nikinenov1601 <nikinenov1@abv.bg>
2024-02-10 21:18:39 +0200

    Adding first problem solution to Object and Classes - Lab

---
+++ 09.Objects and Classes - Lab/01.RandomizeWords/Program.cs
@@ -0,0 +1,18 @@
+﻿
+string[] words = Console.ReadLine().Split(); // Welcome[0] to[1] SoftUni[2] and[3] have[4] fun[5] learning[6] programming[7]
+
+for (int i = 0; i < words.Length; i++) // Length = 8
+{
+
+    Random random = new Random(); // Random sequence generator
+
+    int randonIndex = random.Next(0, words.Length - 1); // [0] -> [7]
+
+    string currentWord = words[i];
+    string wordToSwap = words[randonIndex];
+
+    words[i] = wordToSwap;
+    words[randonIndex] = currentWord;
+}
+
+Console.WriteLine(string.Join(Environment.NewLine, words));
