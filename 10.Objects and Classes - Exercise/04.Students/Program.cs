ce581280e75b88ee8d3231b2a41174cae38af9a6
nikinenov1601 <nikinenov1@abv.bg>
2024-02-11 22:42:53 +0200

    Adding fourth problem solution to Objects and Classes - Exercise

---
+++ 10.Objects and Classes - Exercise/04.Students/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Student
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public double Grade { get; set; }
+
+    public override string ToString()
+    {
+        return $"{FirstName} {LastName}: {Grade:F2}";
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        List<Student> students = new List<Student>();
+
+        for (int i = 0; i < n; i++)
+        {
+            string[] studentInfo = Console.ReadLine().Split();
+            string firstName = studentInfo[0];
+            string lastName = studentInfo[1];
+            double grade = double.Parse(studentInfo[2]);
+
+            students.Add(new Student { FirstName = firstName, LastName = lastName, Grade = grade });
+        }
+
+        students = students.OrderByDescending(s => s.Grade).ToList();
+
+        foreach (Student student in students)
+        {
+            Console.WriteLine(student);
+        }
+    }
+}
