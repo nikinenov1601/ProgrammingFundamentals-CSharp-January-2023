9da96a711a14d4ac0dbe0a57ee1481d72415d184
nikinenov1601 <nikinenov1@abv.bg>
2024-02-11 22:42:24 +0200

    Adding third problem solution to Objects and Classes - Exercise

---
+++ 10.Objects and Classes - Exercise/03.Articles 2.0/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Article
+{
+    public string Title { get; private set; }
+    public string Content { get; private set; }
+    public string Author { get; private set; }
+
+    public Article(string title, string content, string author)
+    {
+        Title = title;
+        Content = content;
+        Author = author;
+    }
+
+    public override string ToString()
+    {
+        return $"{Title} - {Content}: {Author}";
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int numArticles = int.Parse(Console.ReadLine());
+        List<Article> articles = new List<Article>();
+
+        for (int i = 0; i < numArticles; i++)
+        {
+            string[] articleInfo = Console.ReadLine().Split(", ");
+            string title = articleInfo[0];
+            string content = articleInfo[1];
+            string author = articleInfo[2];
+
+            articles.Add(new Article(title, content, author));
+        }
+
+        foreach (Article article in articles)
+        {
+            Console.WriteLine(article);
+        }
+    }
+}
