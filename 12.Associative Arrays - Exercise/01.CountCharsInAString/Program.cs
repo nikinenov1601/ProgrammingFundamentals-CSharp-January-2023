c8734126d7f3449842cae11ba53f4b9c5d6fc90c
nikinenov1601 <nikinenov1@abv.bg>
2024-02-12 21:32:24 +0200

    Adding first problem solution to Associative Arrays - Exercise

---
+++ 12.Associative Arrays - Exercise/01.CountCharsInAString/Program.cs
@@ -0,0 +1,25 @@
+﻿
+string words = Console.ReadLine();
+
+Dictionary<char, int> letters = new Dictionary<char, int>();
+
+foreach (char currentChar in words)
+{
+    if (currentChar == ' ')
+    {
+        continue;
+    }
+
+    if (!letters.ContainsKey(currentChar))
+    {
+        letters[currentChar] = 0;
+    }
+
+    letters[currentChar]++;
+}
+
+foreach (var item in letters)
+{
+
+    Console.WriteLine($"{item.Key} -> {item.Value}");
+}
