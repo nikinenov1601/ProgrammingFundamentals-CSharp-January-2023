4cb3cd7ddaaa27a272ed9bdcb4527bca71aca660
nikinenov1601 <nikinenov1@abv.bg>
2024-02-12 21:24:36 +0200

    Adding third problem solution for Associative Arrays -Lab

---
+++ 11.Associative Arrays - Lab/03.WordSynonyms/Program.cs
@@ -0,0 +1,23 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+Dictionary<string, List<string>> words = new Dictionary<string, List<string>>();
+
+for (int i = 0; i < n; i++)
+{
+    string key = Console.ReadLine();
+    string value = Console.ReadLine();
+
+    if (!words.ContainsKey(key))
+    {
+        List<string> neWords = new List<string>();
+        words.Add(key, neWords);
+    }
+
+    List<string> currentWords = words[key];
+    currentWords.Add(value);
+}
+
+foreach (var word in words)
+{
+    Console.WriteLine($"{word.Key} - {string.Join(", ", word.Value)}");
+}
