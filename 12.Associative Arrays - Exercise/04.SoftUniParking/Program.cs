19e359e61cfb3bf7a48a302d6d043ebc05f1391c
nikinenov1601 <nikinenov1@abv.bg>
2024-02-12 21:33:40 +0200

    Adding fourth problem solution to Associative Arrays - Exercise

---
+++ 12.Associative Arrays - Exercise/04.SoftUniParking/Program.cs
@@ -0,0 +1,37 @@
+﻿
+int n = int.Parse(Console.ReadLine());
+
+Dictionary<string, string> keyValuePairs = new Dictionary<string, string>();
+
+for (int i = 0; i < n; i++)
+{
+    string input = Console.ReadLine();
+    string[] inputSplit = input.Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+    string command = inputSplit[0];
+    string username = inputSplit[1];
+
+    if (!keyValuePairs.ContainsKey(username) && command == "register")
+    {
+        keyValuePairs.Add(username, inputSplit[2]);
+        Console.WriteLine($"{username} registered {inputSplit[2]} successfully");
+    }
+    else if (keyValuePairs.ContainsKey(username) && command == "register")
+    {
+        Console.WriteLine($"ERROR: already registered with plate number {inputSplit[2]}");
+    }
+    else if (!keyValuePairs.ContainsKey(username) && command == "unregister")
+    {
+        Console.WriteLine($"ERROR: user {username} not found");
+    }
+    else if (keyValuePairs.ContainsKey(username) && command == "unregister")
+    {
+        keyValuePairs.Remove(username);
+        Console.WriteLine($"{username} unregistered successfully");
+    }
+}
+
+foreach (var kvp in keyValuePairs)
+{
+    Console.WriteLine($"{kvp.Key} => {kvp.Value}");
+}
