846b0679692d780a3fe7d7d69e2495085efb3a09
nikinenov1601 <nikinenov1@abv.bg>
2024-02-12 21:33:15 +0200

    Adding third problem solution to Associative Arrays - Exercise

---
+++ 12.Associative Arrays - Exercise/03.Orders/Program.cs
@@ -0,0 +1,31 @@
+﻿
+Dictionary<string, double[]> orders = new Dictionary<string, double[]>();
+
+string input = Console.ReadLine();
+
+
+while (input != "buy")
+{
+    string[] inputSplit = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+    string product = inputSplit[0];
+    double price = double.Parse(inputSplit[1]);
+    double quantity = double.Parse(inputSplit[2]);
+
+    if (!orders.ContainsKey(product))
+    {
+        orders[product] = new double[2];
+    }
+
+    orders[product][0] = price;
+    orders[product][1] += quantity;
+
+    input = Console.ReadLine();
+}
+
+foreach (var product in orders)
+{
+    double totalPrice = product.Value[0] * product.Value[1];
+
+    Console.WriteLine($"{product.Key} -> {totalPrice:F2}");
+}
