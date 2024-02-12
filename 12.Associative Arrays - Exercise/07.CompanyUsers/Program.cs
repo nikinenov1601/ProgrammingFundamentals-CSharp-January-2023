176f6fda393ac9ee4e8c228d31d2391f9342849d
nikinenov1601 <nikinenov1@abv.bg>
2024-02-12 21:35:11 +0200

    Adding seventh problem solution to Associative Arrays - Exercise

---
+++ 12.Associative Arrays - Exercise/07.CompanyUsers/Program.cs
@@ -0,0 +1,38 @@
+﻿
+using System.Diagnostics;
+using System.Xml.Linq;
+
+string input = Console.ReadLine();
+Dictionary<string, List<string>> companyUsers = new Dictionary<string, List<string>>();
+
+while (input != "End")
+{
+    string[] inputSplit = input.Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+
+    string companyName = inputSplit[0];
+    string employeeId = inputSplit[1];
+
+    if (!companyUsers.ContainsKey(companyName))
+    {
+        companyUsers[companyName] = new List<string>();
+
+    }
+
+
+    if (!companyUsers[companyName].Contains(employeeId))
+    {
+        companyUsers[companyName].Add(employeeId);
+    }
+
+    input = Console.ReadLine();
+}
+
+foreach (var kvp in companyUsers)
+{
+    Console.WriteLine($"{kvp.Key}");
+
+    foreach (var user in kvp.Value)
+    {
+        Console.WriteLine($"-- {user}");
+    }
+}
