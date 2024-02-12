62f69c232fb4256c3a3436a073e1f7af12e47ac6
nikinenov1601 <nikinenov1@abv.bg>
2024-02-12 21:34:14 +0200

    Adding fifth problem solution to Associative Arrays - Exercise

---
+++ 12.Associative Arrays - Exercise/05.Courses/Program.cs
@@ -0,0 +1,37 @@
+﻿
+string input = Console.ReadLine();
+Dictionary<string, int> courses = new Dictionary<string, int>();
+Dictionary<string, string> studentNames = new Dictionary<string, string>();
+
+while (input != "end")
+{
+    string[] inputSplit = input.Split(':', StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+    string courseName = inputSplit[0];
+    string studentName = inputSplit[1];
+
+    if (courses.ContainsKey(courseName))
+    {
+        courses[courseName]++;
+        studentNames.TryAdd(studentName, courseName);
+    }
+    else
+    {
+        courses.Add(courseName, 1);
+        studentNames.Add(studentName, courseName);
+
+    }
+
+
+    input = Console.ReadLine();
+}
+
+foreach (var course in courses)
+{
+    Console.WriteLine($"{course.Key}: {course.Value}");
+
+    foreach (var student in studentNames.Keys)
+    {
+        Console.WriteLine($"-- {student}");
+    }
+}
