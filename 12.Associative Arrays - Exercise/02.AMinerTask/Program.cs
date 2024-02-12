2614fb5376325f5905658f59a560f07d02069089
nikinenov1601 <nikinenov1@abv.bg>
2024-02-12 21:32:48 +0200

    Adding second problem solution to Associative Arrays - Exercise

---
+++ 12.Associative Arrays - Exercise/02.AMinerTask/Program.cs
@@ -0,0 +1,27 @@
+﻿string resources = Console.ReadLine();
+
+Dictionary<string, int> mine = new Dictionary<string, int>();
+
+while (resources != "stop")
+{
+
+    int quantity = int.Parse(Console.ReadLine());
+
+    if (mine.ContainsKey(resources))
+    {
+        mine[resources] += quantity;
+    }
+
+    else
+    {
+        mine.Add(resources, quantity);
+    }
+
+    resources = Console.ReadLine();
+}
+
+foreach (var kvp in mine)
+{
+
+    Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+}
