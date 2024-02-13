eaf1c4c95c55a540358c13a9fe79a05e3c20f72a
nikinenov1601 <nikinenov1@abv.bg>
2024-02-13 22:28:16 +0200

    Adding fourth problem solution to Text Processing - Lab

---
+++ 13.Text Processing - Lab/04.TextFilter/Program.cs
@@ -0,0 +1,9 @@
+﻿
+string[] bannedWords = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+string text = Console.ReadLine();
+
+foreach (string bannedWord in bannedWords)
+{
+    text = text.Replace(bannedWord, new string('*', bannedWord.Length));
+}
+Console.WriteLine(text);
