8cd011cca78b08a492fca54ad79f8f5fb98aa742
nikinenov1601 <nikinenov1@abv.bg>
2024-02-13 22:27:21 +0200

    Adding second problem solution to Text Processing - Lab

---
+++ 13.Text Processing - Lab/02.RepeatStrings/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System.Text;
+
+string[] words = Console.ReadLine().Split();
+
+StringBuilder result = new StringBuilder();
+
+foreach (string word in words)
+{
+
+    int count = word.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        result.Append(word);
+    }
+
+}
+Console.WriteLine(result);
