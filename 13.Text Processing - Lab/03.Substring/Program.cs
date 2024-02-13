6c75b6dc2102f675118cb8ccae30fb4e12d5ae3d
nikinenov1601 <nikinenov1@abv.bg>
2024-02-13 22:27:48 +0200

    Adding third problem solution to Text Processing - Lab

---
+++ 13.Text Processing - Lab/03.Substring/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Diagnostics.Tracing;
+
+string wordToRemove = Console.ReadLine();
+string word = Console.ReadLine();
+
+while (word.Contains(wordToRemove))
+{
+    int index = word.IndexOf(wordToRemove);
+    word = word.Remove(index, wordToRemove.Length);
+}
+Console.WriteLine(word);
